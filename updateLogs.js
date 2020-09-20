e18e48e579d0013bb51cc08531738dce7ffc465d
vinaycat <vinaymeldrum@gmail.com>
2020-09-20 12:13:43 -0400

    testing

---
+++ updateLogs.js
@@ -0,0 +1,12 @@
+let boolean = false;
+function myFunction(){
+    $('#output').attr("placeholder", "test")
+}
+$("#submit").click(function(){
+    console.log("clicked")
+    //var linkText = $("#output").text();
+    $('#output').attr("placeholder", "test")
+})
+if(boolean){
+    
+}
